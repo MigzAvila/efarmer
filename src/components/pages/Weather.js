2d64256989056075b5c09c377a180b03c18b2916
Ferdi Heredia <ferheredia99@gmail.com>
2021-10-08 00:39:07 -0600

    added and fixed weather page

--- src/components/pages/Weather.js
+++ src/components/pages/Weather.js
@@ -1,40 +1,24 @@
 import React from "react";
 
-const style = {
-  container: {
-    position: "relative",
-    width: "100%",
-    overflow: "hidden",
-    paddingTop: "56.25%" /* 16:9 Aspect Ratio */,
-    margin: "0 auto",
-    textAlign: "center",
-  },
-  iframeStyle: {
-    position: "absolute",
-    top: 0,
-    left: 0,
-    bottom: 0,
-    right: 0,
-    width: "100%",
-    height: "100%",
-    border: "none",
-    margin: "auto",
-  },
+const containerstyle = {
+   textAlign: "center",
+   overflow: "hidden",
+};
+const iframeStyle = {
+  width: "80%",
+  height: "500px",
+  border: "none",
 };
 
 const weather = () => {
   return (
-    <div className={style.iframeStyle}>
+    <div style={containerstyle}>
       <i>
         Weather Page here asdsa dasd sd sa dsa dsa dsa dd asds das ddhgsdfhgad
         dsfasfdsa f asdf asdfsdfdfsd f
-      </i>
+      </i><br/>
       <iframe
-        className={style.iframeStyle}
-        // width="1012"
-        // height="506"
-        width="60%"
-        height="500"
+        style={iframeStyle}
         src="https://www.ventusky.com/?p=17.32;-87.68;8&l=temperature-2m&t=20210903/03"
         title="Ventusky Weather Cast Application"
       ></iframe>
