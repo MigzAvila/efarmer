acc0a80504f7f321376283dd26b35b75d023154e
Miguel Avila <mavila24811@gmail.com>
2021-10-08 22:32:34 -0600

    Added DataTable

--- src/components/pages/DataCenter.js
+++ src/components/pages/DataCenter.js
@@ -1,9 +1,11 @@
 import React from "react";
+import DataTable from "./dataTable/dataTable";
 
 const DataCenter = () => {
   return (
     <div style={{ margin: "0 auto", fontStyle: "italic", textAlign: "center" }}>
       <h1>This is the Data center</h1>
+      <DataTable />
     </div>
   );
 };
