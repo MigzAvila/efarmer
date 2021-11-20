76027d707018a760b638a82cb31da76d35da9b55
Miguel Avila <mavila24811@gmail.com>
2021-11-19 22:31:29 -0600

    Wokring on fourm update replies

--- src/components/ApiCalls/Questions.js
+++ src/components/ApiCalls/Questions.js
@@ -1,40 +1,50 @@
 export class QuestionService {
-    getQuestions() {
-      return fetch("http://localhost:3333/efarmer/")
-        .then((res) => res.json())
-        .then((data) => {
-          return data;
-        });
-    }
+  getQuestions() {
+    return fetch("http://localhost:3333/efarmer/")
+      .then((res) => res.json())
+      .then((data) => {
+        return data;
+      });
+  }
 
-    // updating user
-  async editReply(product) {
+  // updating user
+  async editReply(product, newReply) {
     let state = false;
-    console.log(product, "iddd")
+    console.log(product, "iddd");
+
+    let currentQues = await fetch(`http://localhost:3333/efarmer/${product}`)
+      .then((res) => res.json())
+      .then((data) => {
+        return data;
+      });
+    console.log(currentQues, "currentQues");
+
+    currentQues.Replies.push(newReply);
+
     try {
       await fetch(`http://localhost:3333/efarmer/${product}`, {
         method: "PUT",
         headers: {
-          "Accept": "application/json",
-          "Content-Type": "application/json"
-          },   
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
         body: JSON.stringify({
-          Username: product.Username,
-          Question: product.Question,
-          Replies: product.Replies,
-        })
+          Username: currentQues.Username,
+          Question: currentQues.Question,
+          Replies: currentQues.Replies,
+        }),
       })
-      .then(response => response.json())
-      .then(async (json) => {
-        state = true
-        json = state
-        return json
-      });
+        .then((response) => response.json())
+        .then(async (json) => {
+          state = true;
+          json = state;
+          return json;
+        });
     } catch (err) {
       console.log(err);
       state = false;
     }
-    console.log(state, "is true or false")
+    console.log(state, "is true or false");
     return state;
   }
 }
