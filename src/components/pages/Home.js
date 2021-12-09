cb2dab27eef500c9fb815ae8e935b35c291ecd02
Miguel Avila <mavila24811@gmail.com>
2021-12-08 21:41:46 -0600

    Fix nav bar merge issue

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,39 +1,80 @@
-import React from "react";
-import DoughnutChart from "../charts/doughnutChart";
-import BarChart from "../charts/barChart";
-import Grid from "@mui/material/Grid";
+import React, { useState } from "react";
 import { styled } from "@mui/material/styles";
-import Paper from "@material-ui/core/Paper";
-import Management from "./Management"
+import Box from "@mui/material/Box";
+import NavBar from "./navbar";
+import { BrowserRouter as Router } from "react-router-dom";
+import Routes from "./routes/routes";
 
-const Item = styled(Paper)(({ theme }) => ({
-  ...theme.typography.body2,
-  padding: theme.spacing(1),
-  textAlign: "center",
-  color: theme.palette.text.secondary,
+const DrawerHeader = styled("div")(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  padding: theme.spacing(0, 1),
+
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+  justifyContent: "flex-end",
 }));
 
+const PersistentDrawerLeft = ({
+  setOpenDash,
+  setOpenLogin,
+  userName,
+  setUser,
+}) => {
+  const [drawerWidth, setDrawerWidth] = useState(() => {
+    return 240;
+  });
+  const [open, setOpen] = useState(() => {
+    return false;
+  });
 
-const Home = () => {
+  const handleDrawerOpen = () => {
+    setOpen(!open);
+    setDrawerWidth(() => {
+      return 240;
+    });
+  };
+  const handleDrawerClose = () => {
+    setOpen(!open);
+  };
+
+  const Main = styled("main", { shouldForwardProp: (prop) => prop !== "open" })(
+    ({ theme, open }) => ({
+      flexGrow: 1,
+      padding: theme.spacing(3),
+      transition: theme.transitions.create("margin", {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+      }),
+      marginLeft: `-${drawerWidth}px`,
+      ...(open && {
+        transition: theme.transitions.create("margin", {
+          easing: theme.transitions.easing.easeOut,
+          duration: theme.transitions.duration.enteringScreen,
+        }),
+        marginLeft: 0,
+      }),
+    })
+  );
+
   return (
-    <div>
-      <h1 style={{ textAlign: "center" }}>Reports</h1>
-
-      <Management/>
-      {/* <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-        <Grid item xs={6}>
-          <Item>
-            <DoughnutChart />
-          </Item>
-        </Grid>
-        <Grid item xs={6}>
-          <Item>
-            <BarChart />
-          </Item>
-        </Grid>
-      </Grid> */}
-    </div>
+    <Router>
+      <Box sx={{ display: "flex" }}>
+        <NavBar
+          open={open}
+          handleDrawerOpen={handleDrawerOpen}
+          handleDrawerClose={handleDrawerClose}
+          setOpenDash={setOpenDash}
+          setOpenLogin={setOpenLogin}
+          setUser={setUser}
+          userName={userName}
+        />
+        <Main open={open}>
+          <DrawerHeader />
+          <Routes />
+        </Main>
+      </Box>
+    </Router>
   );
 };
-
-export default Home;
+export default PersistentDrawerLeft;
